8dccc222ac1f830383750daca442064eaceb4296
sylvainbonnot <sppbonnot@gmail.com>
2022-01-27 14:52:58 +0100

    added test

--- streamlitfront/tests/test_base.py
+++ streamlitfront/tests/test_base.py
@@ -1,1 +1,12 @@
 import pytest
+from streamlitfront.base import func_to_page_name
+
+
+def double_func(x: int) -> int:
+    return 2 * x
+
+
+def test_func_to_page_name():
+    result = func_to_page_name(double)
+    expected = "Double func"
+    assert result == expected
