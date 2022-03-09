a322d25bc315c4c896779baba3fb4189a2f024fa
Thor Whalen <thorwhalen1@gmail.com>
2022-03-09 11:45:14 -0800

    refactor: deprecated Command

--- streamlitfront/util.py
+++ streamlitfront/util.py
@@ -1,7 +1,7 @@
 """Utils"""
 from functools import partial
 from typing import TypeVar, Iterable, Dict
-from i2.signatures import Sig, name_of_obj, Command as _Command
+from i2.signatures import Sig, name_of_obj
 
 import streamlit.bootstrap
 
@@ -9,6 +9,7 @@ run_streamlit = partial(
     streamlit.bootstrap.run, command_line="", args=[], flag_options={}
 )
 
+from i2._deprecated import Command as _Command
 
 # TODO: Consider using functools.partial (or subclass thereof) instead of Command
 class Command(_Command):
