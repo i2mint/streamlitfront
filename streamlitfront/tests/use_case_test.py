6df5db7c962654c9b60cecc177311395ac7a373d
Valentin Féron <vferon@pentalog.com>
2022-03-29 17:42:31 -0600

    Restored base_test_use_case

--- streamlitfront/tests/use_case_test.py
+++ streamlitfront/tests/use_case_test.py
@@ -1,10 +1,7 @@
-import pickle
-from xml.etree.ElementPath import prepare_child
+from functools import partial
 import pytest
 from i2 import Sig
-import dill
 from front.tests.test_use_case import base_test_use_case, TEST_USE_CASE_PARAMETER_NAMES, TEST_USE_CASE_PARAMETERS
-from streamlitfront.run_app import run_app
 from streamlitfront.tests.common import (
     compute_output, select_func, send_input,
     dispatch_funcs_with_selenium,
@@ -38,43 +35,10 @@ def mk_ui_func(func, funcs, dom):
     TEST_USE_CASE_PARAMETERS
 )
 def test_use_case(headless, use_case, func_src, kwargs):
-    from meshed.makers import code_to_dag
-    from front.dag import crudify_func_nodes
-    from front.tests.util import get_var_nodes_to_crudify
-
-    dag = code_to_dag(use_case, func_src=func_src)
-    dispatch_func_src = dict(func_src)
-    var_nodes_to_crudify = get_var_nodes_to_crudify(dispatch_func_src.values())
-    front_dag_kwargs = dict(kwargs)
-    if var_nodes_to_crudify:
-        mall = dict()
-        for k, v in kwargs.items():
-            if k in var_nodes_to_crudify:
-                store_key = str(v)
-                mall[f'{k}_store'] = {store_key: v}
-                front_dag_kwargs[k] = store_key
-        crudified_dag = crudify_func_nodes(var_nodes_to_crudify, dag, mall=mall)
-        # assert crudified_dag(**front_dag_kwargs) == dag(**kwargs)
-        dispatch_func_src = {func_node.name: func_node.func for func_node in crudified_dag.func_nodes}
-    dispatch_funcs = list(dispatch_func_src.values())
-    with dispatch_funcs_with_selenium(dispatch_funcs, headless=headless) as dom:
-        front_func_src = {
-            node_name: mk_ui_func(func, dispatch_funcs, dom)
-            for node_name, func in dispatch_func_src.items()
-        }
-        front_dag = code_to_dag(use_case, func_src=front_func_src)
-        assert front_dag(**front_dag_kwargs) == dag(**kwargs)
-    
-
-
-    # funcs = list(func_src.values())
-    # dispatch_funcs = prepare_to_dispatch(funcs)
-    # with dispatch_funcs_with_selenium(dispatch_funcs, headless) as dom:
-    #     base_test_use_case(
-    #         use_case=use_case,
-    #         func_src=func_src,
-    #         inputs=inputs,
-    #         mk_front_func=mk_ui_func,
-    #         funcs=funcs,
-    #         dom=dom,
-    #     )
+    base_test_use_case(
+        use_case=use_case,
+        func_src=func_src,
+        inputs=kwargs,
+        mk_front_func=mk_ui_func,
+        run_front_app=partial(dispatch_funcs_with_selenium, headless=headless),
+    )
