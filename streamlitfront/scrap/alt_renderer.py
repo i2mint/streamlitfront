48894cca5e33425a53a3e6a4641c30ae2b7aa7ff
Thor Whalen <thorwhalen1@gmail.com>
2022-05-19 16:20:02 -0700

    docs: more comments on Renderer

--- streamlitfront/scrap/alt_renderer.py
+++ streamlitfront/scrap/alt_renderer.py
@@ -57,12 +57,14 @@ class Renderer:
 
     # Since self.kwargs is (should) fixed, could curry it out and use curried in
     # __call__ instead of calling _resolve_values_with_state directly (see (*))
-    #         self.resolve_kwargs_with_state = partial(
+    #         self.resolve_inputs = partial(
     #               _resolve_values_with_state, d=kwargs
     #           )
 
     def render(self, state):
-        args, kwargs = _resolve_values_with_state(state, self.args, self.kwargs)  # (*))
+        args, kwargs = _resolve_values_with_state(state, self.args, self.kwargs)
+        # (*) could be this instead:
+        # args, kwargs = self.resolve_inputs(state)
         return self.func(*args, **kwargs)
 
     __call__ = render
