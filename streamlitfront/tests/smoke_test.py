025539863eee71b3e39c77204a11f9a560d9bc73
Thor Whalen <thorwhalen1@gmail.com>
2022-07-22 12:36:03 -0700

    test: smoke_test.py

---
+++ streamlitfront/tests/smoke_test.py
@@ -0,0 +1,7 @@
+"""
+Really quick tests, just to make sure the basics work.
+"""
+
+# Importing examples to make sure that the elements they use still exist
+# (i.e. doesn't raise SyntaxError, ImportError or ModuleNotFound, etc.)
+from streamlitfront.examples import mk_app, custom_output, dag_app, simple_real_audio_ml
