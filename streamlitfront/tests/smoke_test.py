534065e241068e2ced848a9d6e25fb477c4a3922
Thor Whalen <thorwhalen1@gmail.com>
2022-07-25 12:49:53 -0700

    refactor: add pylint ignore to smoke test

--- streamlitfront/tests/smoke_test.py
+++ streamlitfront/tests/smoke_test.py
@@ -4,4 +4,9 @@ Really quick tests, just to make sure the basics work.
 
 # Importing examples to make sure that the elements they use still exist
 # (i.e. doesn't raise SyntaxError, ImportError or ModuleNotFound, etc.)
-from streamlitfront.examples import mk_app, custom_output, dag_app, simple_real_audio_ml
+from streamlitfront.examples import (
+    mk_app,
+    custom_output,
+    dag_app,
+    simple_real_audio_ml,
+)  # pylint: disable=unused-import
