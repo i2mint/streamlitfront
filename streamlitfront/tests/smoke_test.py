5b38ed3ff09914638185390eedd49fdb78d5b52c
Valentin Féron <vferon@pentalog.com>
2022-07-26 09:31:38 -0500

    Moved smoke test to doctest

--- streamlitfront/tests/smoke_test.py
+++ streamlitfront/tests/smoke_test.py
@@ -1,7 +1,12 @@
 """
 Really quick tests, just to make sure the basics work.
-"""
 
-# Importing examples to make sure that the elements they use still exist
-# (i.e. doesn't raise SyntaxError, ImportError or ModuleNotFound, etc.)
-from streamlitfront.examples import mk_app, custom_output, dag_app, simple_real_audio_ml
+Importing examples to make sure that the elements they use still exist
+(i.e. doesn't raise SyntaxError, ImportError or ModuleNotFound, etc.)
+>>> from streamlitfront.examples import (
+...     mk_app,
+...     custom_output,
+...     dag_app,
+...     simple_real_audio_ml
+... )
+"""
