36b5a204eedf651d500a9a5283d804f2d3cf542c
sylvainbonnot <sppbonnot@gmail.com>
2022-11-18 17:36:43 +0100

    update

--- streamlitfront/examples/mk_app.py
+++ streamlitfront/examples/mk_app.py
@@ -12,28 +12,36 @@ def foo(a: int = 1, b: int = 2, c=3):
 
 def bar(x, greeting):
     """bar greets its input"""
-    return f'{greeting} {x}'
+    return f"{greeting} {x}"
 
 
 def confuser(a: int, x: float = 3.14):
-    return (a ** 2) * x
+    return (a**2) * x
 
 
 def proportion(x: int = 100, p: float = 0.5):
     return x * p
+
+
 # ======
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = mk_app(
         [foo, bar, confuser, proportion],
         config={
-            APP_KEY: {'title': 'My app'},
+            APP_KEY: {"title": "My app"},
             RENDERING_KEY: {
-                'foo': {
-                    'execution': {'inputs': {'a': {ELEMENT_KEY: TextInput,}},}
-                },
-                'proportion': {
-                    'execution': {'inputs': {'p': {ELEMENT_KEY: FloatSliderInput,}},}
+                # 'foo': {
+                #     'execution': {'inputs': {'a': {ELEMENT_KEY: TextInput,}},}
+                # },
+                "proportion": {
+                    "execution": {
+                        "inputs": {
+                            "p": {
+                                ELEMENT_KEY: FloatSliderInput,
+                            }
+                        },
+                    }
                 }
             },
         },
