06d534973a30be5cc102c819bff7eb212d953064
Thor Whalen <thorwhalen1@gmail.com>
2022-11-29 14:36:01 +0100

    create store_filtering.py with code

--- streamlitfront/examples/store_filtering.py
+++ streamlitfront/examples/store_filtering.py
@@ -3,18 +3,20 @@
 import re
 from pyckup import grab
 from dol import filt_iter
-
+from i2 import Pipe
 from front import APP_KEY, RENDERING_KEY, ELEMENT_KEY
 
 from streamlitfront.elements import FloatSliderInput
 from streamlitfront.base import mk_app
+from streamlitfront.elements import TextOutput
 
 
+# TODO: Being able to use Pipe to do pre and post processing in front elements
 # TODO: have front (output) elements for iterables (example: paged iteration)
 # TODO: Currying functionality in front
 
 
-def filter_store(store: str, filt: str, flags=0):
+def filter_store(store: str, filt: str, flags: int = 0):
     if isinstance(store, str):
         store_url = store
         store = grab(store_url)
@@ -27,30 +29,27 @@ def filter_store(store: str, filt: str, flags=0):
 
     return filt_iter(store, filt=filt)
 
-
 # filter_store(
 #     '/Users/thorwhalen/Dropbox/_odata/sound/guns', filt='machine'
 # )
 
+# class list_then_text_output(TextOutput):
+#     preprocess()
+from i2 import wrap
+from i2.deco import postprocess
 
 if __name__ == "__main__":
     app = mk_app(
-        [filter_store],
+        # [filter_store],
+        [wrap(filter_store, egress=list)],
+        # [postprocess(list)(filter_store)],
         # config={
         #     APP_KEY: {"title": "My app"},
         #     RENDERING_KEY: {
-        #         # 'foo': {
-        #         #     'execution': {'inputs': {'a': {ELEMENT_KEY: TextInput,}},}
-        #         # },
-        #         "proportion": {
-        #             "execution": {
-        #                 "inputs": {
-        #                     "p": {
-        #                         ELEMENT_KEY: FloatSliderInput,
-        #                     }
-        #                 },
-        #             }
-        #         }
+        #         'filter_store': {
+        #             'execution': {'output': TextOutput},
+        #             # 'execution': {'output': Pipe(list, TextOutput)},
+        #         },
         #     },
         # },
     )
